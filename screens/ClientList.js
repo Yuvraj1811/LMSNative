62efc1a4034fab06baa53660db77518dbb19711b
Yuvraj <ys2029472@gmail.com>
2024-04-01 15:06:36 +0530

    Update

--- screens/ClientList.js
+++ screens/ClientList.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import DropdownComponent from '../components/ProjectDropDown'
 
 const ClientList = () => {
   return (
     <View>
       <Text>ClientList</Text>
+      <DropdownComponent/>
     </View>
   )
 }
